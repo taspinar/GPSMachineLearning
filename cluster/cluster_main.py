7696aeddfa83f4c7968acab97b84bb670dcbb353
rob <romijndersrob@gmail.com>
2017-04-17 11:38:29 +0200

    notebooks for clustering

--- cluster/cluster_main.py
+++ cluster/cluster_main.py
@@ -6,16 +6,25 @@ from scipy.stats import zscore
 from util_cluster import some_cluster
 import glob
 import pandas as pd
+from collections import OrderedDict
 
 INPUT_FOLDER = '../processed_data/'
+HOT = OrderedDict()
+HOT['walk']= np.array([1, 0, 0, 0, 0, 0])
+HOT['train']= np.array([0, 1, 0, 0, 0, 0])
+HOT['subway']= np.array([0, 0, 1, 0, 0, 0])
+HOT['taxi']= np.array([0, 0, 0, 1, 0, 0])
+HOT['bus']= np.array([0, 0, 0, 0, 1, 0])
+HOT['bike']= np.array([0, 0, 0, 0, 0, 1])
+HOT['UNK']= np.array([0, 0, 0, 0, 0, 0])
 
-hot = {'walk': np.array([1, 0, 0, 0, 0, 0]),
-           'train': np.array([0, 1, 0, 0, 0, 0]),
-           'subway': np.array([0, 0, 1, 0, 0, 0]),
-           'taxi': np.array([0, 0, 0, 1, 0, 0]),
-           'bus': np.array([0, 0, 0, 0, 1, 0]),
-           'bike': np.array([0, 0, 0, 0, 0, 1])}
 
+colors = ['r', 'g', 'b', 'y', 'm', 'c', 'k']
+lbl_clr = zip(HOT.keys(), colors)
+title = ''
+for lbl, clr in lbl_clr:
+    title += lbl + '=' +clr + ','
+print(title[:-1])
 def munge():
     def clean_label(label):
         # Pandas treats NaN's as floats. So in the next line we, effectively, check for NaN
@@ -48,7 +57,7 @@ def munge():
         lbl = y[iy]
         # Pandas treats NaN's as floats. So in the next line we, effectively, check for NaN
         if not isinstance(lbl, float):
-            for key, value in hot.items():
+            for key, value in HOT.items():
                 if key in lbl:
                     Y[iy] += value
     return X,Y
@@ -96,21 +105,18 @@ if __name__ == '__main__':
     X, y = munge()
     X,y = remove_outliers(X,y)
     N,D = y.shape
-    
-    funcs = [PCA,tSNE]
-    colors = ['r', 'g', 'b', 'y','m','c','k']
 
     #PCA
     print('----Start PCA----')
     X_red,y_red = PCA(X,y)
     plt.figure()
-    y_color,y_s = some_cluster(y_red)
+    y_color,y_s = some_cluster(y_red, colors)
     plt.scatter(X_red[:, 0], X_red[:, 1],c = y_color,s=y_s ,marker='o', linewidths=0)
-    plt.title('Mode=argmax')
+    plt.title(title)
     plt.savefig('pca.png')
     plt.show(block=True)
     # If the plot doesnt show after above line, then change the MATPLOTLIB settings for your interpreter
-    
+
     plt.figure()
     f4, ax4 = plt.subplots(1, D)
     for d in range(D):
@@ -121,23 +127,29 @@ if __name__ == '__main__':
     plt.savefig('pca_table.png')
     
 
-    #tSNE
-    print('----Start tSNE----')
-    for per in [300]:
-        print('-'*10+'tSNE on perplexity %i'%per + '-'*10)
-        X_red, y_red = tSNE(X, y, per)
-        plt.figure()
-        y_color, y_s = some_cluster(y_red)
-        plt.scatter(X_red[:, 0], X_red[:, 1], c='k', s=y_s, marker='o',
-                    linewidths=0)  # , linewidths=0,c=MNIST_valid[1])
-        plt.title('Mode=argmax')
-        plt.savefig('tSNE%i.png'%per)
-        #
-        plt.figure()
-        f3, ax3 = plt.subplots(1, D)
-        for d in range(D):
-            ax3[d].scatter(X_red[:, 0], X_red[:, 1],c =y_red[:,d] ,marker='*', linewidths=0)
-            ax3[d].set_title('Mode%3i'%d)
-        plt.setp([a.get_xticklabels() for a in ax3], visible=False)
-        plt.setp([a.get_yticklabels() for a in ax3], visible=False)
-        plt.savefig('tsne_table.png')
+    # #tSNE
+    # print('----Start tSNE----')
+    # for per in [300,400,500]:
+    #     print('-'*10+'tSNE on perplexity %i'%per + '-'*10)
+    #     X_red, y_red = tSNE(X, y, per)
+    #     plt.figure()
+    #     y_color, y_s = some_cluster(y_red, colors)
+    #     #color
+    #     plt.scatter(X_red[:, 0], X_red[:, 1], c=y_color, s=y_s, marker='o',
+    #                 linewidths=0)
+    #     plt.title(title)
+    #     plt.savefig('tSNE%i.png' % per)
+    #     #black-white
+    #     plt.scatter(X_red[:, 0], X_red[:, 1], c='k', s=y_s, marker='o',
+    #                 linewidths=0)
+    #     plt.title(title)
+    #     plt.savefig('tSNE_bw%i.png'%per)
+    #     #
+    #     plt.figure()
+    #     f3, ax3 = plt.subplots(1, D)
+    #     for d in range(D):
+    #         ax3[d].scatter(X_red[:, 0], X_red[:, 1],c =y_red[:,d] ,marker='*', linewidths=0)
+    #         ax3[d].set_title('Mode%3i'%d)
+    #     plt.setp([a.get_xticklabels() for a in ax3], visible=False)
+    #     plt.setp([a.get_yticklabels() for a in ax3], visible=False)
+    #     plt.savefig('tsne_table.png')
